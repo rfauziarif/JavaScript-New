5028903901eb1eddf3305b9a1c039279e9823f8c
rfauziarif <muhammadrizkifauziarif@gmail.com>
2022-11-21 15:31:49 +0700

    Membuat tugas 2

---
+++ tugas2.js
@@ -0,0 +1,12 @@
+var number= 1;
+var string= "ini tipe data string";
+var boolean = true ;
+var tnull= "tipeString tapi akan null";
+tnull = null;
+var tarray= [1,2,4,5,7,4]
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(tnull);
+console.log(tarray); 
