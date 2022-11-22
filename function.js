c6f8c60fa7abe48c2fb2c3b50486da7ccd2d218c
rfauziarif <muhammadrizkifauziarif@gmail.com>
2022-11-22 11:08:32 +0700

    menambahkan file function

---
+++ function.js
@@ -0,0 +1,14 @@
+function jalanSatu (){ 
+    console.log ("Jalan satu")
+}
+const jalanDua = function (){
+    console.log("Jalan Dua")
+}
+
+jalanSatu()
+jalanDua()
+
+const jalantiga = ()=>{ 
+    console.log("jalan tiga")
+}
+jalantiga()
